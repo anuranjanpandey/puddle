a36e66b194d8b871f0bab9dfffb4653ffc2b2e82
Anuranjan <anuranjanpandey5@gmail.com>
2024-03-14 00:18:36 -0700

    login and signup

--- core/views.py
+++ core/views.py
@@ -1,8 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from item.models import Category, Item
+
+from . forms import SighnupForm
 
 # Create your views here.
 def index(request):
-    return render(request, 'core/index.html')
+    items = Item.objects.filter(is_sold=False)[0:6]
+    categories = Category.objects.all()
+
+    return render(request, 'core/index.html', {
+        'items': items,
+        'categories': categories
+    })
 
 def contact(request):
     return render(request, 'core/contact.html')
+
+def signup(request):
+    if request.method == 'POST':
+        form = SighnupForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+            return redirect('/login/')
+    else:
+        form = SighnupForm()
+
+    return render(request, 'core/signup.html', {'form': form})  
